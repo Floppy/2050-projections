4ddbb840446eaf7bb97aac8b260b3880efde955b
Martin Keegan <martin.keegan@okfn.org>
2012-03-03 17:09:37 +0000

    make bad changes

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
-$(function () {
-    var d1 = [];
-    for (var i = 0; i < 14; i += 0.5)
-        d1.push([i, Math.sin(i)]);
+var config = {
+  origin: 2012,
+  implementation: 2016,
+  convergence: 2035
+};
 
-    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+var render = function () {
+  var origin = config.origin;
+  var implementation = config.implementation;
+  var convergence = config.convergence;
 
-    // a null signifies separate line segments
-    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-    
-    $.plot($("#placeholder"), [ d1, d2, d3 ]);
-});
+  console.log(implementation);
+
+  
+  var emissions_per_usd = 0.233;
+  var gdp_per_capita = 35000;
+
+  var g_y = 0.02;
+  var g_x = - 0.01;
+
+  var g_dash = -0.0126; // -0.042
+  var g_dash_y = 0.02;
+
+//[i, Math.pow(1 + g, (implementation - i))]);
+
+  var d1 = [];
+  var d2 = [];
+
+  var emissions = function(t) {
+	return emissions_per_usd * Math.pow(1 + g_x, t) * gdp_per_capita * Math.pow(1 + g_y, t);
+  };
+
+  for (var i = origin; i <= implementation; i += 1) {
+    d1.push([i, emissions(i - origin)]);
+  }
+
+  //var x1 = emissions(implementation - origin);
+  var x1 = emissions_per_usd * Math.pow(1 + g_x, (implementation - origin));
+  var gdp_per_capita = gdp_per_capita * Math.pow(1 + g_y, (implementation - origin));
+
+  var emissions = function(t) {
+	return x1 * Math.pow(1 + g_dash, t) * gdp_per_capita * Math.pow(1 + g_dash_y, t);
+  };
+
+  for (var i = implementation; i <= convergence; i += 1) {
+    d2.push([i, emissions(i - implementation)]);
+  }
+
+
+  // a null signifies separate line segments
+  var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+  
+  $.plot($("#placeholder"), [ d1, d2 ]);
+};
+
+var update_controls = function(event, ui, config_var) {
+  config[config_var] = $( "#label-" + event.target.id).text();
+  render();
+}
 
 
 // Initialise UI objects
@@ -17,29 +64,34 @@ $(function() {
 	$( "#impl-year" ).slider({ 
 		max: 2100,
 		min: 2000,
-		value: 2015,
-    	change: function(event, ui) { /* render(); */ },
- 		slide: function(event, ui) { $('#label-' + event.target.id).text(ui.value); }
+		value: 2016,
+      change: function(event, ui) { update_controls(event, ui, 'implementation'); },
+ 	  slide: function(event, ui) { $('#label-' + event.target.id).text(ui.value); }
 	});
 	$( "#conv-year" ).slider({ 
 		max: 2100,
 		min: 2000,
 		value: 2030,
-    	change: function(event, ui) { /* render(); */ },
+    	change: function(event, ui) {  render();  },
  		slide: function(event, ui) { $('#label-' + event.target.id).text(ui.value); }
 	});
 	$( "#target-year" ).slider({ 
 		max: 2100,
 		min: 2000,
 		value: 2050,
-    	change: function(event, ui) { /* render(); */ },
+    	change: function(event, ui) {  render();  },
  		slide: function(event, ui) { $('#label-' + event.target.id).text(ui.value); }
 	});
 	$( "#target-amount" ).slider({ 
 		max: 10,
 		min: 0,
 		value: 1,
-    	change: function(event, ui) { /* render(); */ },
+      change: function(event, ui) {
+		console.log($( "#label-" + event.target.id).text());  
+		console.log("snood");
+		 render();  },
  		slide: function(event, ui) { $('#label-' + event.target.id).text(ui.value); }
 	});
+
+  render();
 });
